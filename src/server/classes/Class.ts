69898a99a5b1e39f966f8a8b42dba86d9538db0f
Theo Last <finchasaurus@gmail.com>
2023-10-25 12:37:51 -0600

    added more transformers

---
+++ src/server/classes/Class.ts
@@ -0,0 +1,11 @@
+export class Class {
+	constructor(private _className: string) {}
+
+	public get className(): string {
+		return this.className;
+	}
+
+	public set className(className: string) {
+		this.className = className;
+	}
+}
--- src/server/services/Server.service.ts
+++ src/server/services/Server.service.ts
@@ -1,4 +1,5 @@
 import { OnStart, OnTick, Service } from "@flamework/core";
+import { UserInputService } from "@rbxts/services";
 
 @Service()
 export class ServerService implements OnStart, OnTick {
